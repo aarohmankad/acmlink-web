1e0c14ec0ce3ecef1eea4bb254540d6edb09f2e8
bball24 <jerry88tan@gmail.com>
2018-02-03 09:24:07 -0800

    Update basic HTML

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ class App extends Component {
         <p className= "App-body">
         Do you have a really long url that needs to be reduced? Enter your url down below!
         </p>
+        <img src= "http://www.emoji.co.uk/files/mozilla-emojis/symbols-mozilla/12113-black-down-pointing-double-triangle.png"
+        className="down-arrow" alt="down-arrow"/>
+        <img src= "http://www.emoji.co.uk/files/mozilla-emojis/symbols-mozilla/12113-black-down-pointing-double-triangle.png"
+        className="down-arrow2" alt="down-arrow2"/>
       </div>
+
+
     );
   }
 }
