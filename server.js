7fb7808c0f20a318d9ec7416a3172e17991d136b
Aaroh Mankad <aarohmankad@gmail.com>
2018-02-04 07:13:54 -0800

    manually build the react app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+const helmet = require('helmet');
+const app = express();
+
+app.use(compression());
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8000);
+console.log(`Listening on port: ${process.env.PORT || 8000}`);
