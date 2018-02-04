0b47f3bc7ba37fd2168074c5dc2be9ba45f4e0cc
Aaroh Mankad <aarohmankad@gmail.com>
2018-02-04 09:26:16 -0800

    remove helmet

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
-const helmet = require('helmet');
 const app = express();
 
 app.use(compression());
-app.use(helmet());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
